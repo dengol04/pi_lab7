4f1eab59a91f758fdee11627f89e684823e4d741
dengol04 <dengol04@mail.ru>
2023-12-28 21:36:00 +0300

    Added third test

--- lab7/Testing/UnitTest1.cs
+++ lab7/Testing/UnitTest1.cs
@@ -13,6 +13,7 @@ namespace Testing
         {
             Assert.IsTrue(Math.Abs(Program.findDistanceBetweenPoints(0, 0, 0, 1) - 1) < double.Epsilon);
             Assert.IsTrue(Math.Abs(Program.findDistanceBetweenPoints(0, 0, 0, -2) - 2) < double.Epsilon);
+            Assert.IsTrue(Math.Abs(Program.findDistanceBetweenPoints(0, 3, 4, 0) - 5) < double.Epsilon);
         }
     }
 }
