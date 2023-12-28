4725022babc47fafa9146384ab421d1a21c29ced
dengol04 <dengol04@mail.ru>
2023-12-28 19:38:54 +0300

    fixed testing

---
+++ lab7/Testing/UnitTest1.cs
@@ -0,0 +1,18 @@
+using lab7;
+namespace Testing
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.IsTrue(Math.Abs(Program.findDistanceBetweenPoints(0, 0, 0, 1) - 1) < double.Epsilon);
+            Assert.IsTrue(Math.Abs(Program.findDistanceBetweenPoints(0, 0, 0, -2) - 2) < double.Epsilon);
+        }
+    }
+}
