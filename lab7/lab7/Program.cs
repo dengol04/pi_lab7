f47929aa6297ec6cd9c49fdd8a65ea95c5665842
dengol04 <dengol04@mail.ru>
2023-12-28 16:00:05 +0300

    Added new C# solution

---
+++ lab7/lab7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{ 
+    internal class Program
+    {
+        static double findDistanceBetweenPoints(double x1, double y1, double x2, double y2) => Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(findDistanceBetweenPoints(0, 0, 0, 1)); // 1
+        }
+    }
+}
